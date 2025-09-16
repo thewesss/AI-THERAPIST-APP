e9f7e1e2ff374d0b9713246070702f7799c78502
Wesley Mutyambizi <wesleymutyambizi@gmail.com>
2025-09-16 16:22:51 +0200

    security

--- ai-therapist-app/backend/src/utils/db.ts
+++ ai-therapist-app/backend/src/utils/db.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"; //provides it a way to define the structure of the app's data and makes it easier to work with mongodb.
 import { logger } from "./logger";
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://wesleymutyambizi_db_user:OXGtbCxmQ4nyW5gH@ai-therapist-agent.7t6s7qs.mongodb.net/?retryWrites=true&w=majority&appName=ai-therapist-agent"
+const MONGODB_URI = process.env.MONGODB_URI || "your-secret-key";
 
 export const connectDB = async () => {
     try {

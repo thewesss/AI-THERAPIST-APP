b2152cc73fb4418ff308093374f9ac13ce80dd8a
Wesley Mutyambizi <wesleymutyambizi@gmail.com>
2025-10-15 19:15:33 +0200

    next

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,11 @@
-// next.config.ts
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   eslint: {
     // Only show warnings, don’t fail production builds
     ignoreDuringBuilds: true,
   },
 };
 
-export default nextConfig;
-
+module.exports = nextConfig;
 
 
